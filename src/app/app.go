3fb920774cef88bd31f58298dbe14a2cc762eef3
NurbekAbilev <abilev.nurbek@gmail.com>
2021-02-07 00:38:12 +0600

    Реализация вебсокетов

--- src/app/app.go
+++ src/app/app.go
@@ -2,17 +2,79 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
 
-func serveStatic() {
-	fmt.Println("Serving static files...")
+//func serveStatic() {
+//	fmt.Println("Serving static files...")
+//	http.Handle("/", http.FileServer(http.Dir("./static")))
+//	http.ListenAndServe(":3000", nil)
+//}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+	//err = ws.WriteMessage(1, []byte("Hi Client!"))
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
+	}
+}
+
+func setupRoutes() {
+	//http.HandleFunc("/", serveStatic)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func main() {
-	go serveStatic()
-	//go nsocket.ServeWebSocket(http.ResponseWriter(), http.Request{})
-	//go nsocket.Example()
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//func main() {
+//	go serveStatic()
+//	http.HandleFunc("/")
+//	//go nsocket.ServeWebSocket(http.ResponseWriter(), http.Request{})
+//	//go nsocket.Example()
+//}
